4b5190fd0dd67ae4c79f51b7334a3f215e03ba8c
AlceuN <74714670+alceunascimento@users.noreply.github.com>
2024-05-19 17:47:00 -0300

    update

--- estatistica/bin/z-knit.R
+++ estatistica/bin/z-knit.R
@@ -1,7 +1,15 @@
-## knit file
-rmarkdown::render(input = "./estatistica/bin/exercicio1.Rmd", 
-                  output_file = "Exerc_1-Descritiva_alceunascimento_20240418.pdf", 
-                  output_dir = "./estatistica/output",
+# knit file
+
+# Gerar a data e hora atual no formato YYYY_MM_DD_HHMMSS
+data_hora_atual <- format(Sys.time(), "%Y_%m_%d_%H%M%S")
+
+# Definir o nome do arquivo de saída incluindo a data e hora atual
+arquivo_saida <- paste0("ufpr_artigo_", data_hora_atual, ".pdf")
+
+#knit
+rmarkdown::render(input = "./ufpr_artigo_template.Rmd", 
+                  output_file = arquivo_saida, 
+                  output_dir = "./output",
                   output_options = list(keep_tex = TRUE)
 )
 
